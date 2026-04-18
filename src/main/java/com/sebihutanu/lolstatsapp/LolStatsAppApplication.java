57e1d9497a057f47f67328f6dd61ff0c9ca3bd93
Hutanu Sebastian <sebastian.hutanu@stud.acs.upb.ro>
2026-04-18 14:54:51 +0300

    Initial commit for project setup

---
+++ src/main/java/com/sebihutanu/lolstatsapp/LolStatsAppApplication.java
@@ -0,0 +1,13 @@
+package com.sebihutanu.lolstatsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolStatsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LolStatsAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sebihutanu/lolstatsapp/LolStatsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebihutanu.lolstatsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LolStatsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
